271550fe7c6efed762e5ae3542f7a2fb2e769f7b
JDJDJD19 <dionsonjeraldranoco@gmail.com>
2022-06-22 05:05:00 +0800

    Configure databse with .env

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,9 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 Dotenv::Railtie.load
+
 
 module DelijeonApi
   class Application < Rails::Application
