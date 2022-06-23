fd83422ba43ebdb1866fe3abd638a21eaf5adc8a
JDJDJD19 <dionsonjeraldranoco@gmail.com>
2022-06-23 13:23:56 +0800

    Create method for registering user and add it on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  resources :users, only: [:create]
   namespace :api do
     resources :categories
     resources :products
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_190816) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_23_050907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_21_190816) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "cart_items", "carts"
